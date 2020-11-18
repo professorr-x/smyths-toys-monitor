f6543b4390b673fbad0019ed1dbb2bf0e9429b2e
hussaiy1 <hussaiy1@gmail.com>
2020-11-18 23:23:08 +0000

    discord and proxy update

--- discord.py
+++ discord.py
@@ -1,22 +1,24 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 webhook = DiscordWebhook(url='https://discord.com/api/webhooks/776417294187757568/f0xqLpTY_ksAeuw8WU__AMtAROp21mBSAmAHzru66n0kbOvNWlpzHVQZ0V18Qou65iPB',
-                         username="Yasser")
+                         username="Smyths Toys Monitor")
 
-
+webhook_blaze = DiscordWebhook(url="https://discord.com/api/webhooks/778745968816291891/dGefV2u6FbAerEBtO85yj17pFL-MrPNwnUig0v2WFwqesWjIGQNn7vI221oWdqNMqeNL",
+                               username="Smyths Toys Monitor")
 
 def send_hook(product_title, url, price, image_url):
     embed = DiscordEmbed(title=product_title,
                          description='PRODUCT NOW AVAIALABLE', color=242424)
-    embed.set_author(name='Yasser\'s Webhook', url=url)
+    embed.set_author(name='Smyths Toys Monitor', url=url)
     #embed.set_footer(text='Embed Footer Text')
     embed.set_timestamp()
     embed.add_embed_field(name='Price', value=price)
-    embed.set_thumbnail(
-        url=image_url)
+    embed.set_thumbnail(url=image_url)
     #embed.add_embed_field(name='Field 2', value='dolor sit')
     #embed.add_embed_field(name='Field 3', value='amet consetetur')
     #embed.add_embed_field(name='Field 4', value='sadipscing elitr')
 
     webhook.add_embed(embed)
     response = webhook.execute()
+    webhook_blaze.add_embed(embed)
+    response = webhook_blaze.execute()
--- monitor.py
+++ monitor.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from discord import send_hook
 from datetime import datetime
+import random
 
 
 pid = ['192247', '192012', '191259', '191430', '193053', '193054']
@@ -11,12 +12,32 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
 }
 
+
+def privateProxies():
+  print('Converting Proxies')
+  with open('proxies.txt') as f:
+    file_content = f.read()
+  file_rows = file_content.split('\n')
+  for i in range(len(file_rows)):
+    if ':' in file_rows[i]:
+      tmp = file_rows[i]
+      tmp = tmp.split(':')
+      proxies = {'http': 'http://' + tmp[2] + ':' + tmp[3] + '@' + tmp[0] + ':' + tmp[1] + '/',
+                 'https': 'http://' + tmp[2] + ':' + tmp[3] + '@' + tmp[0] + ':' + tmp[1] + '/'}
+      proxyList.append(proxies)
+      f.close()
+
+
+proxyList = []
+privateProxies()
+
 while True:
     for i in range(len(pid)):
+        proxy = random.choice(proxyList)
         url = "https://www.smythstoys.com/uk/en-gb/p/{}".format(pid[i])
 
         try:
-            response = requests.get(url, headers=headers)
+            response = requests.get(url, headers=headers, proxies=proxy)
             soup = BeautifulSoup(response.text, 'html.parser')
             stock_status = soup.find('span', attrs={'name': 'js-stockStatusCode'}).get_text()
             price = soup.find('div', attrs={'class': 'price_tag'}).get_text().strip()
